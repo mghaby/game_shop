a00ed9e29098c04372b9c73c588b4e8b6787b9fe
mghaby <mark.ghaby71@hotmail.com>
2021-02-22 12:32:59 +1100

    graphic stuff on launch of app

--- index.rb
+++ index.rb
@@ -1,1 +1,16 @@
-puts "Hello whatever"
+puts "_    _      _ _       
+| |  | |    | | |      
+| |__| | ___| | | ___  
+|  __  |/ _ \ | |/  _ \\ 
+| |  | |  __/ | | \(_\) |
+|_|  |_|\___|_|_|\_____/ 
+"
+puts "
+
+
+Welcome to Mark's Game Store!"
+puts "Here we specialise in Consoles, Games and Peripherals"
+
+
+
+
