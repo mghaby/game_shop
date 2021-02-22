486076e945d655d0ad34eb83a46a6a903662a313
mghaby <mark.ghaby71@hotmail.com>
2021-02-22 13:08:28 +1100

    more ascii and entry ui stuff added

--- index.rb
+++ index.rb
@@ -1,3 +1,6 @@
+require "tty-prompt"
+prompt = TTY::Prompt.new
+
 puts "_    _      _ _       
 | |  | |    | | |      
 | |__| | ___| | | ___  
@@ -10,7 +13,24 @@ puts "
 
 Welcome to Mark's Game Store!"
 puts "Here we specialise in Consoles, Games and Peripherals"
+sleep(3)
+puts ""
+puts "
+_._._                       _._._
+_|   |_                     _|   |_
+| ... |_._._._._._._._._._._| ... |
+| ||| |  o   Game Store   o | ||| |
+| ... |  ...    ...    ...  | ... |
+|[-|-]| [-|-]  [-|-]  [-|-] |[-|-]|  
+|     |---------------------|     | 
+| ... |  ...    ...    ...  | ... |
+|[-|-]|  :::   .-+-.   :::  |[-|-]|
+|     | |~|~|  |_|_|  |~|~| |     |
+|_____|_|__|_|_|_|_|__|_|_|_|_____|
+              /======\\
 
+"
+# ASCII ART ABOVE BY Sher
 
-
+prompt.yes?("Would you like to enter Mark's Game Store?")
 
