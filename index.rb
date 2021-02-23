c9afa5496c88702f9cf2eee311e97db42835f9e9
mghaby <mark.ghaby71@hotmail.com>
2021-02-23 13:09:19 +1100

    json gem added, json is parsed into index, logic changes to selection loop

--- index.rb
+++ index.rb
@@ -1,5 +1,11 @@
 require "tty-prompt"
+require 'json'
 prompt = TTY::Prompt.new
+leave = "Thanks for visiting Mark's Game Store!"
+consoles_hash = JSON.parse(File.read('./db/consoles.json'))
+games_hash = JSON.parse(File.read('./db/games.json'))
+peripherals_hash = JSON.parse(File.read('./db/peripherals.json'))
+
 
 puts "_    _      _ _       
 | |  | |    | | |      
@@ -32,5 +38,22 @@ _|   |_                     _|   |_
 "
 # ASCII ART ABOVE BY Sher
 
-prompt.yes?("Would you like to enter Mark's Game Store?")
+yn = prompt.yes?("Would you like to enter Mark's Game Store?")
+case yn
+when true
+    selection = prompt.select("What would you like to purchase?", %w(Consoles Games Peripherals))
+    case selection
+    when "Consoles"
+        # tty table stuff here nicely 
+        puts consoles_hash
 
+    when "Games"
+        puts "Games"
+
+    when "Peripherals"
+        puts "peripheral"
+    end
+    
+when false
+    puts leave
+end 
