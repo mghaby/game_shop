6f9d6dcf9e4bd5d274fcdbcaac05a7d11422ebb6
mghaby <mark.ghaby71@hotmail.com>
2021-03-01 14:34:26 +1100

    update to notes file

--- notes.rb
+++ notes.rb
@@ -1,12 +1,3 @@
-## if purchase with no items render an error thing
-
-### if consoles limit to 1 purchase per customer
-
-#### if purchasing games Economies of Scale discounts:
-#### 3 games 10% off total
-#### 5 games 15% off total
-#### 10 games 25% off 
-
 # on purchase generate a reciept and calculate total price, render and end
 
 # being able to go back if click on games instead of console or whatever 
