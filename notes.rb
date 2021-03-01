20f19ed592263aa718f8ca8977f6a86784c8737e
mghaby <mark.ghaby71@hotmail.com>
2021-03-01 20:47:00 +1100

    updated notes.rb

--- notes.rb
+++ notes.rb
@@ -1,9 +1,5 @@
 # being able to go back if click on games instead of console or whatever 
 
-# discount calcs are broken bc if @games is empty array nil cant + but if its 0 by default it cant
-# have math done on it proper coz it uses 0 index in calcs
-
-
 #  enter = fine
 #  what to purchase should have checkout
 ## consoles / games / peripherals should have "go back option"
